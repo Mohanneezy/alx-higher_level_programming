e191a3fa238379133bc402941d0097aa90382159
Mohanneezy <lilnahublacksmith@gmail.com>
2022-05-31 11:58:43 +0300

    this is the first commit for the pyton project

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-x = number % 10
+x = abs(number) % 10
+if number < 0:
+    x = -x
 if x > 5:
     print("Last digit of", number, "is", x, "and is greater than 5")
 elif x == 0:
