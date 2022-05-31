ab6074c0969eb191f8feed03080fd31e89496968
Mohanneezy <lilnahublacksmith@gmail.com>
2022-05-31 13:54:46 +0300

    this is the first commit for the pyton project

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     for i in range(97, 123):
-    if i == ord(c):
-    print("{} ".format(chr(i)), end="")
+      if i == ord(c):
+          return True
+      else:
+          return False
