7e2c1cfc3700ebbff6f619941d54ea17735f2ef8
Mohanneezy <lilnahublacksmith@gmail.com>
2022-05-31 14:19:15 +0300

    this is the first commit for the pyton project

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,4 +3,4 @@ def uppercase(str):
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
             i =ord(c) -32
-                print("{}".format(chr(i)), end="")
+            print("{}".format(chr(i)), end="")
