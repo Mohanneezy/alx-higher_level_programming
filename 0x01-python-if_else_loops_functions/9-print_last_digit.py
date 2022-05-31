e46676315e74a5cc37b7fe513e6324c73af89ffc
Mohanneezy <lilnahublacksmith@gmail.com>
2022-05-31 14:38:35 +0300

    this is the first commit for the pyton project

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-x = abs(number) % 10
-if number < 0:
-    x = -x
-    print("{}".format(x), end="")
+    x = abs(number) % 10
+    if number < 0:
+        x = -x
+        print("{}".format(x), end="")
