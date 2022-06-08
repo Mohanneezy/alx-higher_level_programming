58976e2be93ab24c23b196d8dfcdf67c9bc55ea4
Mohanneezy <lilnahublacksmith@gmail.com>
2022-06-08 09:35:27 +0300

    this is a commit for the pyton project

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 # 3-common_elements.py
-# Brennan D Baraban <375@holbertonschool.com>
 
 
 def common_elements(set_1, set_2):
-    for i in range(len(set_1)):
-      for j in range(len(set_2)):
-        if set_1[i] == set_2[j]:
-    return (set_1[i])
+    return (set_1)
