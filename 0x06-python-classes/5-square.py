2c37fd5c06e9950e5b9bd9c3fc4e77f865220091
Mohanneezy <lilnahublacksmith@gmail.com>
2022-06-21 21:13:40 +0300

    this is a commit for the pyton project

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+    def __init__(self, size=0):
+        """Initialize a new Square."""
+        self.size = size
+
+    def size(self):
+        """Get/set the current size of the square."""
+        return (self.__size)
+
+
+    def size(self, value):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
+		
+	def my_print(self):
+       """Print the square with the # character."""
+       for i in range(0, self.__size):
+           [print("#", end="") for j in range(self.__size)]
+           print("")
+       if self.__size == 0:
+            print("")
