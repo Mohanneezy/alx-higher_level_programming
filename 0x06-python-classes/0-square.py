0cd9f91fe53854c5d8bfd4e2c6e1afed960750c2
Mohanneezy <lilnahublacksmith@gmail.com>
2022-06-21 19:44:33 +0300

    this is a commit for the pyton project

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" this a class """
+# 0-square.py
+# Nahom Gulte <lilnahublacksmith@gmail.com>
+""" this is a class """
 
 class Square:
     """this defines a square"""
--- 0x06-python-classes/0-square.py.bak
+++ 0x06-python-classes/0-square.py.bak
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+""" this a class """
 
 class Square:
+    """this defines a square"""
     pass
