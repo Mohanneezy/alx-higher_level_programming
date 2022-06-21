f5a06d587bff6684d9aac3af042c7fc621eddfc3
Mohanneezy <lilnahublacksmith@gmail.com>
2022-06-21 20:26:23 +0300

    this is a commit for the pyton project

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,9 +3,8 @@
 """Define a class Square."""
 
 
-class Square(size):
-        def __init__(self, size):
-        """Initialize a new Square.
-
-        """
+class Square:
+    """Represent a square."""
+    def __init__(self, size):
+        """Initialize a new Square."""
         self.size = size
