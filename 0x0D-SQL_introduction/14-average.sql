094145fde945550f18f911cd384442592825b000
Mohanneezy <lilnahublacksmith@gmail.com>
2022-08-09 23:20:19 +0300

    this is a commit  for the project of introduction to sql

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
 -- count number of time of occurence of a record in table second_table
-SELECT AVG(`score`) x FROM `second_table`;
+SELECT AVG(`score`) AS `average` FROM `second_table`;
--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- count number of time of occurence of a record in table second_table
-SELECT `score`,COUNT(*)  x FROM `second_table` GROUP BY `score` ORDER BY x DESC;
+SELECT `score`,COUNT(*) AS `value` FROM `second_table` GROUP BY `score` ORDER BY `value` DESC;
