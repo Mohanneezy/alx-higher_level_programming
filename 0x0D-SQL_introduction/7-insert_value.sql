8ad1d14fd98608346c325021f91fbb67783596dc
Mohanneezy <lilnahublacksmith@gmail.com>
2022-08-09 21:18:41 +0300

    this is a commit  for the project of introduction to sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- list all rows of the table first_table
-INSERT INTO  `first_table` SET(`id`,`name`) VALUES (`89`,`Best School`);
+INSERT INTO  `first_table` (`id`,`name`) VALUES (`89`,`Best School`);
 
