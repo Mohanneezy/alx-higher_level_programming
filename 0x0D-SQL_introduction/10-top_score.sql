dd16235793c53e767eb5171f835c7f2673b002da
Mohanneezy <lilnahublacksmith@gmail.com>
2022-08-09 23:31:40 +0300

    this is a commit  for the project of introduction to sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,5 @@
---Selecting records of the table second_table
-SELECT score, name FROM second_table ORDER BY score4 DESC;
+-- Lists all records of the table second_table.
+-- Records are ordered by descending score.
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
