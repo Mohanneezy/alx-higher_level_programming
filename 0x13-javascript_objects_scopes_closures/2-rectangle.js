90e5d5de3eff3c7228dff862ec4b3342ec27cf0f
Mohanneezy <lilnahublacksmith@gmail.com>
2022-09-06 16:13:38 +0300

    java script with rectangle class with width condition

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0)module.exports = class empty {};
+    if (w <= 0 || h <= 0)
+      this.width = w;
+      this.height = h;
   }
 };
