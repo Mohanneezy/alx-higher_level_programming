9f6226cc45c6b8c5c019804e0aa99fab9693ffd9
Mohanneezy <lilnahublacksmith@gmail.com>
2022-10-18 21:38:09 +0300

    commit for java sript web sraping

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,9 +3,9 @@ const request = require('request');
 int count = 0;
 let url = 'https://swapi-api.hbtn.io/api/films/';
 request(url, function (error, response, body) {
-  if(JSON.parse(body).characters == "https://swapi-api.hbtn.io/api/people/18/")
+  if(JSON.parse(body).result.characters == 'https://swapi-api.hbtn.io/api/people/18/')
     {
       count = count + 1;	    
     }
-      console.log(error || count);
+      console.log(count);
 });
