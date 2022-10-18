d26e9f5305dc6a869061aef9b094d4805e5ff5e8
Nahom.Gulte <nahomg@awash.local>
2022-10-18 14:44:14 +0300

    this is a commit for the project java script web scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
 fs.readFile(process.argv[2],'utf-8', (err, inputD) => {
-   if (err) throw err;
-      console.log(inputD.toString());
+   if (err) throw err;console.log(inputD.toString());
 });
